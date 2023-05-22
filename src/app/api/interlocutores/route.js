a541496236d2fd758f4e6f8aa74dce2351cde8ac
julio <julioroja987@gmail.com>
2023-05-22 00:22:51 -0300

    The api route for interlocutors was created for the POST method. It is now successfully inserted into the database

---
+++ src/app/api/interlocutores/route.js
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import { pool } from "../../../../database/db";
+
+export async function GET(request) {
+    try {
+        const query = "SELECT * FROM `interlocutores`";
+        const [tipo_interlocutores] = await pool.query(query);
+        return NextResponse.json(tipo_interlocutores);
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+export async function POST(request) {
+    try {
+        const { id_interlocutor, tipo_interlocutor, nombre, dni, fecha_alta } =
+            await request.json();
+        const query = `INSERT INTO interlocutores (id_interlocutor, tipo_interlocutor, nombre, dni, fecha_alta) VALUES (?, ?, ?, ?, ?)`;
+        const values = [
+            id_interlocutor,
+            tipo_interlocutor,
+            nombre,
+            dni,
+            fecha_alta,
+        ];
+        const [interlocutores] = await pool.query(query, values);
+        return NextResponse.json(interlocutores);
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+// Parece que aun no anda el DELETE en esta version da error "Unexpected end of JSON input"
+
+// export async function DELETE(request) {
+//     try {
+//         const { id_interlocutor } = await request.json();
+//         console.log(id_interlocutor);
+
+//         // const query = `DELETE FROM interlocutores WHERE id_interlocutor = ?`;
+//         // const values = [id_interlocutor];
+//         // const [interlocutorBorrado] = await pool.query(query, values);
+//         return NextResponse.json(id_interlocutor);
+//     } catch (error) {
+//         console.log(error);
+//         // return NextResponse.json({ error: error.message }, { status: 500 });
+//     }
+// }
+
+// if (!id_interlocutor) {
+//     return NextResponse.json(
+//         { error: "ID de interlocutor no proporcionado" },
+//         { status: 400 }
+//     );
+// }
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,46 +1,30 @@
 import React from "react";
+import { getTiposInterlocutores } from "../../../lib/get-data";
 
-const page = () => {
+const page = async () => {
+    const tiposInterlocutores = await getTiposInterlocutores();
     return (
         <section className="container mx-auto max-w-5xl">
-            <h1 className="mb-8 text-center text-6xl font-bold">Alta</h1>
-            {/* Aca pongo un h-auto mientras luego agrego contenido*/}
-            <div className="flex h-[calc(100vh-240px)] items-center justify-center">
-                <div className="flex h-full w-1/4 flex-col items-center justify-center gap-4 rounded-l-2xl bg-hdgray/10">
-                    <button className="h-10 w-32 rounded-sm bg-hdgray text-white">
-                        Empresa
-                    </button>
-                    <button className="h-10 w-32 rounded-sm bg-hdgray text-white">
-                        Conductor
-                    </button>
-                    <button className="h-10 w-32 rounded-sm bg-hdgray text-white">
-                        Camión
-                    </button>
-                    <button className="h-10 w-32 rounded-sm bg-hdgray text-white">
-                        Acoplado
-                    </button>
-                </div>
-                <form
-                    action=""
-                    className="flex h-full w-3/4 flex-col items-center justify-center gap-4 rounded-r-2xl bg-hbrightgray/10 text-xl"
-                >
-                    <div className="flex flex-col bg-red-300/10 p-12">
-                        <label htmlFor="">Nombre y apellido</label>
-                        <input type="text" className="w-40" placeholder="" />
-                        <label htmlFor="">DNI</label>
-                        <input type="text" className="w-40" placeholder="" />
-                    </div>
-                    <button
-                        type="submit"
-                        className="h-10 w-32 rounded-sm bg-hdgray text-white"
-                    >
-                        Guardar
-                    </button>
-                </form>
-                {/* <div className="flex h-full w-3/4 items-center justify-center rounded-r-2xl bg-hbrightgray/10 text-xl font-medium">
-                    <h2>Seleccione una opción para dar de alta</h2>
-                </div> */}
-            </div>
+            <h1 className="mb-8 text-center text-4xl font-bold">Alta</h1>
+            <select name="" id="" className="">
+                <option id="0" value="empty">
+                    Seleccione una opcion
+                </option>
+                {tiposInterlocutores.map(({ id_interlocutores, tipo }) => {
+                    return (
+                        <option
+                            id={id_interlocutores}
+                            key={id_interlocutores}
+                            value={tipo}
+                        >
+                            {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
+                        </option>
+                    );
+                })}
+            </select>
+            <input type="text" placeholder="Nombre / Placa" />
+            <input type="text" placeholder="DNI / Patente" />
+            <button type="submit">Crear</button>
         </section>
     );
 };
