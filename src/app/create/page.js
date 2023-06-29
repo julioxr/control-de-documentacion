31f6263435f08b8d5ce30fc061a3bc0161634954
julio <julioroja987@gmail.com>
2023-06-29 11:31:01 -0300

    Session verification was added for the /create route.

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -9,9 +9,12 @@ const Create = () => {
     const router = useRouter();
     const [tiposInterlocutores, setTiposInterlocutores] = useState([]);
 
-    // if (session.status === "unauthenticated") router.push("/");
-
     useEffect(() => {
+        if (session.status === "unauthenticated") {
+            router.push("/");
+            return;
+        }
+
         const fetchTiposInterlocutores = async () => {
             try {
                 const response = await fetch(
@@ -29,30 +32,32 @@ const Create = () => {
         fetchTiposInterlocutores();
     }, [tiposInterlocutores]);
 
-    return (
-        <section className="container mx-auto max-w-5xl">
-            <h1 className="mb-8 text-center text-4xl font-bold">Alta</h1>
-            <select name="" id="" className="">
-                <option id="0" value="empty">
-                    Seleccione una opcion
-                </option>
-                {tiposInterlocutores.map(({ id_interlocutores, tipo }) => {
-                    return (
-                        <option
-                            id={id_interlocutores}
-                            key={id_interlocutores}
-                            value={tipo}
-                        >
-                            {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
-                        </option>
-                    );
-                })}
-            </select>
-            <input type="text" placeholder="Nombre / Placa" />
-            <input type="text" placeholder="DNI / Patente" />
-            <button type="submit">Crear</button>
-        </section>
-    );
+    if (session.status === "authenticated") {
+        return (
+            <section className="container mx-auto max-w-5xl">
+                <h1 className="mb-8 text-center text-4xl font-bold">Alta</h1>
+                <select name="" id="" className="">
+                    <option id="0" value="empty">
+                        Seleccione una opcion
+                    </option>
+                    {tiposInterlocutores.map(({ id_interlocutores, tipo }) => {
+                        return (
+                            <option
+                                id={id_interlocutores}
+                                key={id_interlocutores}
+                                value={tipo}
+                            >
+                                {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
+                            </option>
+                        );
+                    })}
+                </select>
+                <input type="text" placeholder="Nombre / Placa" />
+                <input type="text" placeholder="DNI / Patente" />
+                <button type="submit">Crear</button>
+            </section>
+        );
+    }
 };
 
 export default Create;
